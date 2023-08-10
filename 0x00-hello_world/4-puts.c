441850bf5fa31aadeb766b824fe2e83cd3a4790d
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-10 15:32:18 -0700

    C tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/*hello*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzz
+le");
 return (0);
 }  
