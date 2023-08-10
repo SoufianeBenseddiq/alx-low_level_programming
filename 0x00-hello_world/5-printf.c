c672e562e1cda62066b883a81e5bb7b7e1883d17
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-10 16:00:52 -0700

    C tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+i/**
+  * main - A program that print a line using printf function
+  * return: 0 (Success)
+  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
 
