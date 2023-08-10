5558c16171da10a76a167e9f70dd1759b2ccfffd
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-10 15:39:57 -0700

    C tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//main function puts a phrase and returns 0
+/*main function puts a phrase and returns 0*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
