9e142edf5c8cd83a2675a7768df98e9f8dabfe34
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-11 03:54:30 -0700

    project 2 in C

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of two two-digit numbers,
+ *              separated by a space and comma, in ascending order.
+ *              Certain combinations are considered the same.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 98; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');           
+if (i < 98 || j < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
