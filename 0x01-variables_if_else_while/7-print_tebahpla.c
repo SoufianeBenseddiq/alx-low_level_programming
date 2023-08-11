e86a6388c5420d9e88f8b141a419a1f11df025e0
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-11 03:26:06 -0700

    project 2 in C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#!/include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet in reverse order,
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lowercase='z';
+while (lowercase >= a)
+{
+putchar(lowercase);
+lowercase--;
+}
+putchar("\n");
+return(0);
+}
+
