ac8bece7c2a835a2b8922584989a9444d3fbd9e0
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-15 07:58:08 -0700

    0x02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char lowcase;
+for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+{
+_putchar(lowcase);
+}
+_putchar('\n');
+}
+
