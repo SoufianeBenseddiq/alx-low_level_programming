403ba309f2d5e8dc4d65598c9c1428616aa4c4d3
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-17 05:58:13 -0700

    0x04-more_functions_nested_loops tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,14 +4,13 @@
 * @n: input variable
 * Return: void
 */
-void print_diagonal (int n)
+void print_diagonal(int n)
 {
 int i, j;
 if (n <= 0)
 _putchar('\n');
 for (i = 0; i < n; i++)
 {
-}
 for (j = 0; j < i; j++)
 {
 _putchar(' ');
