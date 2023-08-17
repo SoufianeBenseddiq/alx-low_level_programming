13c7fcda1ff8713ca4dcd132adac16211770c550
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-17 07:23:18 -0700

    0x04-more_functions_nested_loops tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_most_numbers - Prints 1356789
  *
@@ -7,13 +6,12 @@
  */
 void print_most_numbers(void)
 {
-    char i;
-
-    for (i = '0'; i <= '9'; i++)
-    {
-        if (i != '2' || i != '4)
-            _putchar(i);
-    }
-    _putchar('\n');
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '2' && i != '4')
+_putchar(i);
+}
+_putchar('\n');
 }
 
