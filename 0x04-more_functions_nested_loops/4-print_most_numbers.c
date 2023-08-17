1ec5b873c46db28023c705c39adc7fe23a9d5041
SoufianeBenseddiq <soufianebenseddiq8@gmail.com>
2023-08-17 05:10:42 -0700

    0x04-more_functions_nested_loops tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * print_numbers - Prints digits from 0 to 9
+ * print_most_numbers - Prints digits from 0 to 9 except 2 and 4
  *
  * Return: No return value (void)
  */
 void print_most_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-if (i != 2 && i != 4)
-_putchar(i);
-}
-_putchar('\n');
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i != 2 && i != 4)
+            _putchar(i + '0'); // Convert integer to character using '0' offset
+    }
+    _putchar('\n'); // Print a newline character
 }
 
